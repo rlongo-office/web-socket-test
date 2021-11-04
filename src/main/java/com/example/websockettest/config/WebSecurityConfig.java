a512926d0982903d88404a7de6858b88ca7e7cd6
rlongo-dev <56315160+rlongo-dev@users.noreply.github.com>
2021-11-04 18:05:06 -0400

    testing security including login authentication

--- src/main/java/com/example/websockettest/config/WebSecurityConfig.java
+++ src/main/java/com/example/websockettest/config/WebSecurityConfig.java
@@ -1,20 +1,66 @@
 package com.example.websockettest.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //@Autowired
+    //private AppUserDetailsService userDetailsService;
+
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         // This is not for websocket authorization, and this should most likely not be altered.
         http
                 .httpBasic().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                //.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
-                .antMatchers("/game-app").permitAll();
+                .anyRequest()
+                .fullyAuthenticated();
+                //.antMatchers("/game-app").permitAll();
                 //.anyRequest().denyAll();
     }
+    /*
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+        // Tell Spring to ignore securing the handshake endpoint. This allows the handshake to take place unauthenticated
+        web.ignoring().antMatchers("/stomp/**");
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
+    }
+
+    // Create an AuthenticationManager bean to Authenticate users in the ChannelInterceptor
+    @Bean
+    public AuthenticationManager authManager() throws Exception {
+
+        return this.authenticationManager();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+
+        return new BCryptPasswordEncoder(10);
+
+    }
+    */
 }
