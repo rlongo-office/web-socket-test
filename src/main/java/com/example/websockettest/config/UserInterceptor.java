1046c6afa4b964d096b4e08e39499a6c9b87eeef
rlongo-dev <56315160+rlongo-dev@users.noreply.github.com>
2021-11-07 20:18:48 -0500

    Added test security code to autho and authenticate the user on login

--- src/main/java/com/example/websockettest/config/UserInterceptor.java
+++ src/main/java/com/example/websockettest/config/UserInterceptor.java
@@ -11,19 +11,8 @@ import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.stereotype.Component;
 
-@Component
 public class UserInterceptor implements ChannelInterceptor {
-    private static final String USERNAME_HEADER = "login";
-    private static final String PASSWORD_HEADER = "passcode";
-    private final WebSocketAuthenticatorService webSocketAuthenticatorService;
-    @Inject
-    public UserInterceptor(final WebSocketAuthenticatorService webSocketAuthenticatorService) {
-        this.webSocketAuthenticatorService = webSocketAuthenticatorService;
-    }
-
     @Override
     public boolean preReceive(MessageChannel channel) {
         return ChannelInterceptor.super.preReceive(channel);
@@ -42,21 +31,16 @@ public class UserInterceptor implements ChannelInterceptor {
 
         if (StompCommand.CONNECT.equals(accessor.getCommand())) {
             Object raw = message.getHeaders().get(SimpMessageHeaderAccessor.NATIVE_HEADERS);
-            final String username = accessor.getFirstNativeHeader(USERNAME_HEADER);
-            final String password = accessor.getFirstNativeHeader(PASSWORD_HEADER);
-            final UsernamePasswordAuthenticationToken user = webSocketAuthenticatorService.getAuthenticatedOrFail(username, password);
 
-            /*
             if (raw instanceof Map) {
                 Object name = ((Map) raw).get("username");
-
 
                 if (name instanceof ArrayList) {
                     String username = ((ArrayList<String>) name).get(0).toString();
                     User user = new User(username);
                     accessor.setUser(user);
-
-            }}*/
+                }
+            }
         }
         return message;
     }
